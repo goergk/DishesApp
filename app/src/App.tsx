be6c8b73e27bcf1acac6b804e65b95b9890fdd8b
goergk <grzegorzkaw124@gmail.com>
2022-06-22 14:07:36 +0200

    Installed Tailwind & prepared structure

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='w-[100%] h-[100vh] flex justify-center items-center'>
+      <h1 className='font-sans font-light text-[4rem]'>Setup done</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,svg}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Noto Sans TC', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: [],
+}
